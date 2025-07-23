973fdad89a3ca8645d82c088289fe8fcbe577442
Agnes Triselia Yudia <agnes.rikta@gmail.com>
2025-07-23 11:45:02 +0700

    Initial Commit

---
+++ MazzApp/MazzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazzAppApp.swift
+//  MazzApp
+//
+//  Created by Phincon on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MazzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
