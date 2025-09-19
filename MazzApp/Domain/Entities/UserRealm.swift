bf1ec907fa75c1dd542427abbd4f172919fc3335
Agnes Triselia Yudia <agnes.rikta@gmail.com>
2025-09-19 14:38:30 +0700

    Delete unuseful code on UserRealm

--- MazzApp/Domain/Entities/UserRealm.swift
+++ MazzApp/Domain/Entities/UserRealm.swift
@@ -5,21 +5,6 @@
 //  Created by Agnes Triselia Yudia on 19/09/25.
 //
 
-//import Foundation
-//import RealmSwift
-//
-//class User: Object, Identifiable {
-//    @objc dynamic var id: ObjectId = ObjectId.generate()
-//    @objc dynamic var fullname: String = ""
-//    @objc dynamic var username: String = ""
-//    @objc dynamic var email: String = ""
-//    @objc dynamic var password: String = ""
-//    
-//    override static func primaryKey() -> String? {
-//        return "id"
-//    }
-//}
-
 import Foundation
 import RealmSwift
 
